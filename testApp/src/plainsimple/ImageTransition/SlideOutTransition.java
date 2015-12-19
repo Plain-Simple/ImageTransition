6176f8b269a031ecc6e166c9052efff8946bb430
stefan4472 <stefan.kussmaul@gmail.com>
2015-12-18 22:53:46 -0500

    Fixed NonPush Transition, Some Coding Improvements

--- testApp/src/plainsimple/ImageTransition/SlideOutTransition.java
+++ testApp/src/plainsimple/ImageTransition/SlideOutTransition.java
@@ -25,7 +25,7 @@ public class SlideOutTransition {
     // percentage a row should slide across screen before next row starts moving
     private float threshold;
     // whether start image shoulc be "pushed" off the screen
-    private boolean pushOffScreen = true;
+    private boolean pushOffScreen = false;
     // whether or not transition has been completed
     private boolean transitionFinished = false;
     // whether or not transition animation is currently playing
@@ -71,14 +71,17 @@ public class SlideOutTransition {
     }
 
     // renders and returns next frame in sequence
-    public Bitmap nextFrame() { // todo: require start?
+    public Bitmap nextFrame() { 
+        if(frameCounter == 0) {
+            start();
+        }
         if (frameCounter < totalFrames) {
             frameCounter++;
-        } else if (frameCounter == totalFrames) {
-            transitionFinished = true;
-            isPlaying = false;
+            return getFrame(frameCounter);
+        } else {
+            stop();
+            return endImage;
         }
-        return getFrame(frameCounter);
     }
 
     // renders and returns frame based on completion of sequence
@@ -93,27 +96,27 @@ public class SlideOutTransition {
         } else {
             // draw full rows, simply transferring from endImage to startImage
             int full_rows = (int) (completion * numRows);
-            Rect src_end = new Rect(0, 0, imgWidth, full_rows * row_height);
-            this_frame.drawBitmap(endImage, src_end, src_end, null);
+            Rect end_src = new Rect(0, 0, imgWidth, full_rows * row_height);
+            this_frame.drawBitmap(endImage, end_src, end_src, null);
 
             // calculate percentage of current row completed
             float row_completion = (completion - (full_rows / (float) numRows)) * (float) numRows;
 
             if (pushOffScreen) {
                 // shift current row of start image left, taking 1 - row_completion as width
-                Rect src_start = new Rect((int) (imgWidth * (1.0 - row_completion)), full_rows * row_height,
+                Rect start_src = new Rect((int) (imgWidth * row_completion), full_rows * row_height,
                         imgWidth, (full_rows + 1) * row_height);
-                Rect dst_start = new Rect(0, src_start.top, src_start.width(), src_start.bottom);
-                this_frame.drawBitmap(startImage, src_start, dst_start, null);
+                Rect start_dst = new Rect(0, start_src.top, start_src.width(), start_src.bottom);
+                this_frame.drawBitmap(startImage, start_src, start_dst, null);
 
                 // take leading portion from endImage and transfer to trailing portion of start image
-                src_end = new Rect(0, full_rows * row_height, (int) (row_completion * imgWidth), (full_rows + 1) * row_height);
-                Rect dst_end = new Rect(imgWidth - src_end.width(), src_end.top, imgWidth, src_end.bottom);
-                this_frame.drawBitmap(endImage, src_end, dst_end, null);
+                end_src = new Rect(0, full_rows * row_height, (int) (row_completion * imgWidth), (full_rows + 1) * row_height);
+                Rect end_dst = new Rect(imgWidth - end_src.width(), end_src.top, imgWidth, end_src.bottom);
+                this_frame.drawBitmap(endImage, end_src, end_dst, null);
             } else {
-                // simply transfer from trailing portion of end image to start image
-                src_end = new Rect(0, full_rows * row_height, (int) (row_completion * imgWidth), (full_rows + 1) * row_height);
-                this_frame.drawBitmap(endImage, src_end, src_end, null);
+                // simply transfer trailing portion of end image to start image
+                end_src = new Rect((int) ((1.0 - row_completion) * imgWidth), full_rows * row_height, imgWidth, (full_rows + 1) * row_height);
+                this_frame.drawBitmap(endImage, end_src, end_src, null);
             }
         }
         return workingFrame;
