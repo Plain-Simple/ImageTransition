b7dab67a237fbdb8a45de133228da3a04184e34c
stefan4472 <stefan.kussmaul@gmail.com>
2015-12-27 18:47:55 -0500

    Slide In Animation (No Push) Working

--- testApp/src/plainsimple/ImageTransition/SlideInTransition.java
+++ testApp/src/plainsimple/ImageTransition/SlideInTransition.java
@@ -3,6 +3,7 @@ package plainsimple.ImageTransition;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Rect;
+import android.util.Log;
 
 /**
  * Created by Stefan on 12/19/2015.
@@ -27,7 +28,6 @@ public class SlideInTransition extends ImageTransition {
     public void drawFrame(float completion, Canvas canvas) {
         int row_height = imgHeight / numRows;
         int threshold_width = (int) (imgWidth * threshold);
-
         if(completion >= 1.0) {
             canvas.drawBitmap(endImage, 0, 0, null);
         } else if(completion <= 0.0) {
@@ -37,26 +37,25 @@ public class SlideInTransition extends ImageTransition {
             float total_thresholds = (numRows - 1) + 1.0f / threshold;
             float num_thresholds = total_thresholds * completion;
             // draw from bottom to top
-            for(int i = numRows - 1; i >= 0; i--) { // todo: prevent from slowing down: never draw more than full screen
+            for(int i = 0; i < numRows; i++) {
                 // represents section of the row on canvas that is in transition
-                Rect src = new Rect(0, (i - 1) * row_height,
-                        (int) ((num_thresholds - (i - (numRows - 1))) * threshold_width),
-                        imgHeight - i * row_height);
+                Rect src = new Rect(0, (numRows - i - 1) * row_height,
+                        (int) ((num_thresholds - i) * threshold_width), (numRows - i) * row_height);
                 // limit to width of screen
                 if(src.width() > imgWidth) {
                     src.right = imgWidth;
                 }
                 if(pushOffScreen) {
-                    // pixels from startImage to be shifted left
-                    Rect start_src = new Rect(src.width(), src.top, imgWidth, src.bottom);
+                    // pixels from startImage to be shifted right
+                    Rect start_src = new Rect(0, src.top, imgWidth - src.width(), src.bottom);
                     // new location of shifted pixels
-                    Rect start_dst = new Rect(0, start_src.top, start_src.width(), start_src.bottom);
+                    Rect start_dst = new Rect(imgWidth - start_src.width(), start_src.top, imgWidth, start_src.bottom);
                     canvas.drawBitmap(startImage, start_src, start_dst, null);
 
                     // pixels from endImage to be drawn on canvas
-                    Rect end_src = new Rect(0, src.top, src.width(), src.bottom);
+                    Rect end_src = new Rect(imgWidth - src.width(), src.top, imgWidth, src.bottom);
                     // new location of pixels on canvas
-                    Rect end_dst = new Rect(src.left, src.top, src.right, src.bottom);
+                    Rect end_dst = new Rect(0, src.top, end_src.width(), src.bottom);
                     canvas.drawBitmap(endImage, end_src, end_dst, null);
                 } else {
                     // simply transfer pixels from endImage to startImage. No change necessary.
